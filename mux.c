cc4470dfc189d92ac672a6ffcc194ecca4cbb39b
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2016-07-29 15:53:14 -0400

    More mux code.

--- mux.c
+++ mux.c
@@ -2,3 +2,6 @@
   PORT->Group[0].PINCFG[PIN_PA14].bit.PMUXEN = 1;
   PORT->Group[0].PMUX[PIN_PA14/2].reg &= 0xF0;
   PORT->Group[0].PMUX[PIN_PA14/2].reg |= 0x07;
+  
+     PORT->Group[0].PINCFG[PIN_PA14].reg &=~ (uint8_t)(PORT_PINCFG_INEN) ;
+     PORT->Group[0].DIRSET.reg |= (uint32_t)(1<<14) ;
